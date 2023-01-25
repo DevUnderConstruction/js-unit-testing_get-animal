e01b3ecceab507cefbf53c14f26f2c3a48daa60b
Jonas Meidinger <jomeidinger@gmx.de>
2023-01-25 17:31:06 +0100

    testing completed

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-export function getAnimal() {}
+export function getAnimal(pet) {
+  if (pet === "cats") {
+    return "I totally love " + pet + "!";
+  } else if (pet === "dogs") {
+    return "I like " + pet + "!";
+  }
+  return "I do not like animals at all!";
+}
+
+console.log(getAnimal("cats"));
